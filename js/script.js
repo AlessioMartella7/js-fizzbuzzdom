72ded4b65ef7d83b35694318e2d172c59aeee5e4
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-02 18:00:48 +0200

    printed in page the elements

--- js/script.js
+++ js/script.js
@@ -17,48 +17,54 @@ Come abbiamo visto ci sono usare varie tecniche (style , className, classList
 */
 
 /*
-1-scriviamo un ciclo for che stampi i numeri da 1 a 100 in console.
-2-isoliamo i multipli di 3 e stampiamo al suo posto Fizz.
-3-isoliamo i multipli di 5 e stampiamo al suo posto Buzz.
+1-creiamo un contenitore nel DOM e importiamolo in JS.
+2-creiamo una variabile d'appoggio per i dati da stampare in pagina.
+3-scriviamo un ciclo for che stampi i numeri da 1 a 100 in console.
 4-isoliamo i multipli sia di 3 che di 5 e stampiamo al loro posto FizzBuzz.
-5-creiamo un contenitore nel DOM e importiamolo in JS.
-6-creiamo una variabile d'appoggio per i dati da stampare in pagina.
+5-isoliamo i multipli di 3 e stampiamo al suo posto Fizz.
+6-isoliamo i multipli di 5 e stampiamo al suo posto Buzz.
 7-riempiamo la pagina con gli elementi JS creati.
 8-impostiamo uno stile ai vari elementi da noi creati inserendoli nel DOM usando JS(numeri,fizz,buzz,fizzbuzz).
 */
 
-//6 - creiamo una variabile d'appoggio per i dati da stampare in pagina.
 
+//1 - creiamo un contenitore nel DOM e importiamolo in JS.
+const board = document.getElementById('board')
 
+//2 - creiamo una variabile d'appoggio per i dati da stampare in pagina.
+let varia=0;
 
-//1 - scriviamo un ciclo "for" che stampi i numeri da 1 a 100 in console.
-
+//3 - scriviamo un ciclo for che stampi i numeri da 1 a 100 in console
 for(let i = 1; i <=100; i++) {
+    varia=i;
     
-//2 - isoliamo i multipli di 3 e stampiamo al suo posto fizz   
+//4 - isoliamo i multipli di 3 e 5 stampiamo al suo posto FizzBuzz   
 
-    if((i % 3 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
-    console.log('Fizz')
-    }
+    if(i % 3 === 0 &&  i % 5 === 0) {
+    console.log('FizzBuzz');
+    varia = "FizzBuzz";
+    board.innerHTML +=`<div class="">${varia}</div>`;
 
-//3 - isoliamo i multipli di 5 e stampiamo al suo posto Buzz.
+//5 - isoliamo i multipli di 3 e stampiamo al suo posto Fizz
+    }else if(i % 3 === 0){
+        console.log('Fizz');
+        varia = 'Fizz';
+        board.innerHTML +=`<div class="">${varia}</div>`;
 
-    if((i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
-    console.log('Buzz')
-    }  
-    
-//4 - isoliamo i multipli sia di 3 che di 5 e stampiamo al loro posto FizzBuzz.
-    
-    if(i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz')
-    }
+//6 - isoliamo i multipli di 5 e stampiamo al suo posto Buzz
+    }else if(i % 5 === 0){
+        console.log('Buzz');
+        varia = 'Buzz';
+        board.innerHTML +=`<div class="">${varia}</div>`;
 
-    else if ( !(i % 3 === 0 || i % 5 === 0) ) {
+    }else{
         console.log(i);
+        board.innerHTML +=`<div class="">${varia}</div>`;
+
+
     }
 
 }
-//5 - creiamo un contenitore nel DOM e importiamolo in JS
+    
 
-const board = document.getElementById('board')
 
