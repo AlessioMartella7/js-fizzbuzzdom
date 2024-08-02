96dc4bc57739a938078d78e432bebcd6e1d61507
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-02 17:30:19 +0200

    adding variables

--- js/script.js
+++ js/script.js
@@ -27,23 +27,27 @@ Come abbiamo visto ci sono usare varie tecniche (style , className, classList
 8-impostiamo uno stile ai vari elementi da noi creati inserendoli nel DOM usando JS(numeri,fizz,buzz,fizzbuzz).
 */
 
+//6 - creiamo una variabile d'appoggio per i dati da stampare in pagina.
+
+
+
 //1 - scriviamo un ciclo "for" che stampi i numeri da 1 a 100 in console.
 
 for(let i = 1; i <=100; i++) {
-
+    
 //2 - isoliamo i multipli di 3 e stampiamo al suo posto fizz   
 
     if((i % 3 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
     console.log('Fizz')
     }
 
-//3- isoliamo i multipli di 5 e stampiamo al suo posto Buzz.
+//3 - isoliamo i multipli di 5 e stampiamo al suo posto Buzz.
 
     if((i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
     console.log('Buzz')
     }  
     
-//4- isoliamo i multipli sia di 3 che di 5 e stampiamo al loro posto FizzBuzz.
+//4 - isoliamo i multipli sia di 3 che di 5 e stampiamo al loro posto FizzBuzz.
     
     if(i % 3 === 0 && i % 5 === 0) {
         console.log('FizzBuzz')
@@ -53,7 +57,8 @@ for(let i = 1; i <=100; i++) {
         console.log(i);
     }
 
-    
-
-
 }
+//5 - creiamo un contenitore nel DOM e importiamolo in JS
+
+const board = document.getElementById('board')
+
